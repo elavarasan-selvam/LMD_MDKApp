22e865f1c6e9b68452a02a6c90e8ef572766345e
d prasanna <d.laxmi.prasanna@accenture.com>
2025-08-12 08:31:53 +0000

    Base LMD App

---
+++ Application.app
@@ -0,0 +1,11 @@
+{
+	"_Name": "LMD_MDKApp",
+	"Version": "/LMD_MDKApp/Globals/Application/AppDefinition_Version.global",
+	"MainPage": "/LMD_MDKApp/Pages/Main.page",
+	"OnLaunch": "/LMD_MDKApp/Rules/Service/Initialize.js",
+	"OnWillUpdate": "/LMD_MDKApp/Rules/Application/OnWillUpdate.js",
+	"OnDidUpdate": "/LMD_MDKApp/Rules/Service/Initialize.js",
+	"Styles": "/LMD_MDKApp/Styles/Styles.less",
+	"Localization": "/LMD_MDKApp/i18n/i18n.properties",
+	"_SchemaVersion": "25.6"
+}
---
+++ Rules/Service/Initialize.js
@@ -0,0 +1,30 @@
+export default function Initialize(context) {
+
+    // Perform pre data initialization task
+
+    // Initialize all your Data sources
+    let _API_LASTMILEVISITLIST = context.executeAction('/LMD_MDKApp/Actions/API_LASTMILEVISITLIST/Service/InitializeOffline.action');
+    let _API_LASTMILERELOADREQUEST = context.executeAction('/LMD_MDKApp/Actions/API_LASTMILERELOADREQUEST/Service/InitializeOffline.action');
+    let _DEST_SAMLMD_PPROP = context.executeAction('/LMD_MDKApp/Actions/DEST_SAMLMD_PPROP/Service/InitializeOffline.action');
+
+    //You can add more service initialize actions here
+
+    return Promise.all([_API_LASTMILEVISITLIST, _API_LASTMILERELOADREQUEST, _DEST_SAMLMD_PPROP]).then(() => {
+        // After Initializing the DB connections
+
+        // Display successful initialization  message to the user
+        return context.executeAction({
+
+            "Name": "/LMD_MDKApp/Actions/GenericToastMessage.action",
+            "Properties": {
+                "Message": "Application Services Initialized",
+                "Animated": true,
+                "Duration": 1,
+                "IsIconHidden": true,
+                "NumberOfLines": 1
+            }
+        });
+    }).catch(() => {
+        return false;
+    });
+}
